39f64b144be6ef66c7ca47eec5a85afe6222b4fe
krish732006 <gandhikrish305@gmail.com>
2026-05-06 18:12:40 +0530

    fix: stream chunk handling final fix

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -335,56 +335,31 @@ function Chat() {
       ]);
 
       try {
-        // let buffer = "";
-
         while (true) {
           const { done, value } = await reader.read();
           if (done) break;
 
-          // buffer += decoder.decode(value);
-
-          // let parts = buffer.split("__CHAT_ID__:");
-
-          // // last part incomplete hoi shake
-          // buffer = parts.pop();
-
-          // for (let part of parts) {
-          //   aiText += part;
-          //   updateLastMessage(aiText);
-
-          //   const idMatch = buffer.match(/^[a-f\d]{24}/);
-          //   if (idMatch) {
-          //     currentChatId = idMatch[0];
-          //     setActiveChatId(currentChatId);
-          //     localStorage.setItem("activeChatId", currentChatId);
-          //   }
-          // }
-
-          // if (!buffer.includes("__CHAT_ID__")) {
-          //   aiText += buffer;
-          //   updateLastMessage(aiText);
-          //   setPartialAI(aiText);
-          //   buffer = "";
-          // }
-
           const chunk = decoder.decode(value);
 
-          // 🔥 handle chatId
-          if (chunk.includes("__CHAT_ID__:")) {
-            const [textPart, idPart] = chunk.split("__CHAT_ID__:");
+          // 🔥 CHECK CHAT ID
+          if (chunk.includes("__CHAT_ID__")) {
+            const parts = chunk.split("__CHAT_ID__:");
 
-            aiText += textPart;
-            updateLastMessage(aiText);
+            // text part
+            if (parts[0]) {
+              aiText += parts[0];
+              updateLastMessage(aiText);
+            }
 
-            if (idPart) {
-              currentChatId = idPart.trim();
+            // id part
+            if (parts[1]) {
+              currentChatId = parts[1].trim();
               setActiveChatId(currentChatId);
               localStorage.setItem("activeChatId", currentChatId);
             }
           } else {
             aiText += chunk;
             updateLastMessage(aiText);
-            setPartialAI(aiText);
           }
         }
 
@@ -465,7 +440,7 @@ function Chat() {
       });
 
       // 🔥 COMPLETE → reset
-      setPartialAI("");
+      // setPartialAI("");
 
       await axios.get(`${BASE_URL}/api/chat/${userName}`).then((res) => {
         setChats(res.data);
